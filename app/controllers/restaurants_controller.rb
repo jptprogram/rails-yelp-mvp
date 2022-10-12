92dfce2567756d4539a1f2659a6ad1bd2772e4b1
JuanPedro Toro <jpdungeon@gmail.com>
2022-10-11 23:25:15 -0300

    faltan las views

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,26 @@
+class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :adsress, :phone_number, :category)
+  end
+
+end
